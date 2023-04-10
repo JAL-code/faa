4bcce3d53deee8b014fff7f1edd3b5ed4cec4ced
JAL-code <70151426+JAL-code@users.noreply.github.com>
2023-04-10 19:58:43 -0400

    General Update
    Updates to improve documentation. Working files are noted with "_w" at end of file name.  High level action added to review contents. Added variable 'intent' to toggle indentation.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# FAA
-A demonstration of using skills with python and json files using [FAA Documentation](https://www.faa.gov/data_research/aviation_data_statistics/data_downloads).
+# Sorting FAA Documentation
+A demonstration of using python and json files to view [FAA Documentation](https://www.faa.gov/data_research/aviation_data_statistics/data_downloads).
 
 ## List of Documents
 1. ECFR Content: Displaying title 14, up to date as of 3/30/2023. Title 14 was last amended 3/21/2023. Loaded 4/3/2023.
 2. SDR: Year 2023. Loaded 4/3/2023.  See Filelayout.txt for explaination of fields.
 
-## Actions:
+## Actions Completed:
 1. Convert tab delimited text file to json. See **tab_delimit_to_json.py**.
-2. Convert block json to space delimited json for easy reading.  **See edit_json.py**.
+2. Convert default json  file to space delimited json file for easy reading.  **See edit_json.py**.
+
+## Actions in Progress:
+1. Load json file and review contents.
+
+### Notes
+1. Working files are noted with a "_w" at end of file name.
+
--- tab_delimit_to_json.py
+++ tab_delimit_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Code by JAL-code
-# Process the document for supermarket items.
+# Create a json file from tab delimited text file.
 # Note this file is stage 2 for updating json formatting.
 
 import os
@@ -25,7 +25,7 @@ def file_exists(filename, location):
 
 # load the lines of data into a dictionary list
 def load_products(file_name):
-    print("Loading the products")
+    print("Loading the data")
     # , encoding='utf8 ' replaced by 2nd line
     with open(file_name, 'r' , encoding='utf8') as data_file:  
         data_loaded = data_file.readlines()
@@ -45,26 +45,37 @@ def load_products(file_name):
         processed_data.append(row)    
     return processed_data
 
-# save an indent formatted json file.
+# save an indent formatted json file.  Size is larger.
 def save_list_of_items(data, datasave):
     with open(datasave, 'w') as file:
         json.dump(data, file, indent=4)
 
+# save a json file for a compact size.
+def save_compact_list_of_items(data, datasave):
+    with open(datasave, 'w') as file:
+        json.dump(data, file)
+
 if __name__ == '__main__':
+    indent = False
     # set the file names
     # set the record file
     record_file='sdr2023g.txt'
     # set the working file.  If working file is corrupted, reload the record file.
-    working_file='sdr2023g_w.json'
+    working_file='sdr2023g_c.json'
     # set dir location
     working_dir = os.getcwd()
     relative_location = f"\\faa\\"
-    # does this file exist the working folder?
+    # does this file exist in the working folder?
     default_folder = f"{working_dir}\\"
     print(f"{default_folder}\\{record_file}")
     if file_exists(record_file, default_folder):
         print("We can load the data!")
         data = load_products(f"{default_folder}{record_file}")
-        save_list_of_items(data, f"{default_folder}{working_file}")
-    if file_exists(working_file, default_folder)==False:
+        if indent: 
+            save_list_of_items(data, f"{default_folder}{working_file}")
+        if not indent:
+            save_compact_list_of_items(data, f"{default_folder}{working_file}")
+    if not file_exists(working_file, default_folder):
         print("We must find the record file!")
+
+#  Regex101 candidate: \s(?P<column>[c]+[\d]{1,3})\s+|(?P<columntype>[a-zA-Z]+)\s+|(?P<length>[\d]+)|(?P<description>\b[A-Za-z]+\s.*?\.\b)
